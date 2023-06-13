2d2e452f6f59d55c02d58b7f8287eb33443d153b
AndreyBry <andrey062006@bk.ru>
2023-06-13 19:26:52 +0500

    git commit -m 'Homework'

---
+++ homework.py
@@ -0,0 +1,11 @@
+def palindrome(s):
+    """
+    Полученная на входе строка переворачивается и сравнивается с исходной, если они равны, то возвращается True, иначе - False
+    :param s: str
+    :return: True or False
+    """
+    return True if s == s[::-1] else False
+
+
+print(palindrome('лепсспел'))
+print(palindrome('helloworld'))
