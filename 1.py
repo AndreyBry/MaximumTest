de7ebf6db54d54e21480d9fe3c8eab42afabf1e6
AndreyBry <andrey062006@bk.ru>
2023-06-20 18:07:42 +0500

    lesson2

--- 1.py
+++ 1.py
@@ -1,3 +1,17 @@
+# посчитать количество каждой из букв в строке
+
+def strcounter(s):
+    symbols_counter = {}
+    for symbol in s:
+        symbols_counter[symbol] = symbols_counter.get(symbol, 0) + 1
+
+    return symbols_counter
+
+
+for key, value in strcounter('aaaabbbbbuuuudsfsfsdf').items():
+    print(f'{key}: {value}')
+
+"""Homework"""
 def palindrome(s):
     """
     Полученная на входе строка переворачивается и сравнивается с исходной,
