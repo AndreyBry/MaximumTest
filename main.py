f04e80a066c95baf21ddf40379952ad6c8421768
AndreyBry <andrey062006@bk.ru>
2023-06-27 17:06:14 +0500

    main

--- main.py
+++ main.py
@@ -10,3 +10,19 @@ def strcounter(s):
 
 for key, value in strcounter('aaaabbbbbuuuudsfsfsdf').items():
     print(f'{key}: {value}')
+
+"""HOMEWORK"""
+
+
+def palindrome(s):
+    """
+    Полученная на входе строка переворачивается и сравнивается с исходной,
+    если они равны, то возвращается True, иначе - False
+    :param s: str
+    :return: True or False
+    """
+    return True if s == s[::-1] else False
+
+
+print(palindrome('лепсспел'))
+print(palindrome('helloworld'))
