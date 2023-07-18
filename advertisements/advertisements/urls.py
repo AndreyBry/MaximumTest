80710fb8e27c7c4a16b4c12121b4dc94f8af825b
AndreyBry <andrey062006@bk.ru>
2023-07-18 14:47:37 +0500

    homework

--- advertisements/advertisements/urls.py
+++ advertisements/advertisements/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('app_advertisements.urls')),
+    path('app_advertisements', include('app_advertisements.urls')),
+    path('', include('app_lesson_4.urls')),
 ]
